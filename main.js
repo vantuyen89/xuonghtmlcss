6b41b63b63d551b97719ace1e2e9edab3ed0d674
vantuyen89 <vantuyenfpl@gmail.com>
2024-06-10 15:10:26 +0700

    shop-page

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function () { 
+    $(".my-rating").starRating({
+        totalStars: 5,
+        starShape: "rounded",
+        strokeColor: "#894A00",
+        strokeWidth: 10,
+        starSize: 25,
+    });
+});
